1f3a3c7bd52cd589004e2b16c28173949883e246
Ayato_Etherias <bellyukari123@gmail.com>
2024-06-12 02:16:41 -0600

    avanze dia 12/06/2024

---
+++ app/src/main/java/com/example/pmo12024p/ActivityInit.java
@@ -0,0 +1,55 @@
+package com.example.pmo12024p;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ActivityInit extends AppCompatActivity
+{
+
+    EditText nombres, apellidos, edad, correo;
+    Button btnagregar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_init);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+
+        
+        nombres = (EditText) findViewById(R.id.Nombres);
+        apellidos = (EditText) findViewById(R.id.apellidos);
+        edad = (EditText) findViewById(R.id.edad);
+        correo = (EditText) findViewById(R.id.correo);
+        btnagregar = (Button) findViewById(R.id.btn);
+
+        btnagregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Agregar();
+
+            }
+        });
+
+
+
+
+    }
+
+    private void Agregar() {
+    }
+}
